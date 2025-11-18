63682a72e48fbd86c9b31074a9fcc91ffee89399
JotaveCoimbra <joaovitorcoimbrasilva807@gmail.com>
2025-11-18 10:12:15 -0300

    Matriz6x6

---
+++ matriz6x6.ino
@@ -0,0 +1,37 @@
+int linhas[6]  = {25,26,27,14,12,13};          // OUTPUT
+int colunas[6] = {32,33,34,35,36,39};         // ADC
+
+void setup() {
+  Serial.begin(115200);
+
+  for (int i = 0; i < 6; i++) {
+    pinMode(linhas[i], OUTPUT);
+    digitalWrite(linhas[i], LOW);  // começa tudo em LOW
+  }
+  // colunas: não precisa pinMode pra analogRead
+}
+
+void loop() {
+  for (int l = 0; l < 6; l++) {
+
+    // ativa só essa linha
+    digitalWrite(linhas[l], HIGH);
+    delayMicroseconds(150);  // dá tempo estabilizar
+
+    for (int c = 0; c < 6; c++) {
+      int valor = analogRead(colunas[c]);
+      Serial.print("Linha ");
+      Serial.print(l);
+      Serial.print(" | Coluna ");
+      Serial.print(c);
+      Serial.print(" = ");
+      Serial.println(valor);
+    }
+
+    digitalWrite(linhas[l], LOW);
+    delay(50);
+  }
+
+  Serial.println("---------");
+  delay(300);
+}
